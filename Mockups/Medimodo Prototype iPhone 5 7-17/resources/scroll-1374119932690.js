dcc1adfca7510a87aa0ec0d10ccbf5dcd2fb1df2
Mike P. Sinn <mike@thinkbynumbers.org>
2013-07-25 00:20:12 -0500

    Added Mockup Images to Repository

---
+++ Mockups/Medimodo Prototype iPhone 5 7-17/resources/scroll-1374119932690.js
@@ -0,0 +1,22 @@
+(function(window, undefined) {
+
+    /*********************** START STATIC ACCESS METHODS ************************/
+
+    jQuery.extend(jimMobile, {
+        "loadScrollBars": function() {
+            jQuery(".s-b54fe8ba-ea4a-40db-ae0e-c2ef60eaf22b .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-ce10269c-1500-4f7b-9b12-0b09449b2e44 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-119b1d6a-9db6-4ab1-944c-f0c489d4f3f1 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-33335c32-44b9-45bd-82e8-172d63682b2b .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-d12245cc-1680-458d-89dd-4f0d7fb22724 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-50584143-37aa-4b04-85fc-c6f8ad0222ad .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-d6620091-eb8f-4484-9b8b-88ca4f070653 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-350d7657-17d2-4e10-b729-cb33be7f5c73 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-ea409544-cb51-4797-846e-8b545595c832 .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+            jQuery(".s-7ca5df9b-efa3-4461-a3cb-bb4042faf38e .ui-page").overscroll({ showThumbs:true, direction:'multi' });
+         }
+    });
+
+    /*********************** END STATIC ACCESS METHODS ************************/
+
+}) (window);
---
+++ Mockups/Medimodo Prototype iPhone 5 7-17/review/includes/include-1374119932690.js
@@ -0,0 +1,4 @@
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/_jim/javascript/jim-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/_jim/javascript/mobile/ios/iphone/jim-iphone-min.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/scroll-1374119932690.js'></script>");
+document.write("<script type='text/javascript' charset='utf-8' src='./resources/prototype-1374119932690.js'></script>");
