206d8a6b470281b2c1d5838a4e8a31989ee692a3
Imara <tiana.imara@outlook.com>
2013-08-09 15:49:39 +0200

    Layout for schedule dialog
    Removed dialog header
    Layout for "Every x days" option
    Layout for "Monthly" option

--- src/com/quantimodo/medication/dialogs/SetScheduleDialog.java
+++ src/com/quantimodo/medication/dialogs/SetScheduleDialog.java
@@ -3,10 +3,14 @@ package com.quantimodo.medication.dialogs;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.animation.AnimationUtils;
 import android.widget.CompoundButton;
+import android.widget.TableLayout;
 import android.widget.ViewFlipper;
 import com.quantimodo.medication.R;
 import com.quantimodo.medication.Utils;
@@ -14,6 +18,9 @@ import com.quantimodo.medication.things.MedicationSchedule;
 import org.holoeverywhere.LayoutInflater;
 import org.holoeverywhere.app.AlertDialog;
 import org.holoeverywhere.widget.*;
+
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class SetScheduleDialog
 {
@@ -25,7 +32,10 @@ public class SetScheduleDialog
 	//private LinearLayout lnInterval;
 	private ViewFlipper viewFlipper;
 	private CheckBox[] cbDays = new CheckBox[8];
+    private View[] btnCalendarDays = new View[31];
+    private HashMap<Integer, Boolean> selectedCalendarDays;
 	private Spinner spScheduleType;
+    private Spinner spStartDay;
 
 	private boolean ignoreCheckBoxChange = false;
 
@@ -41,7 +51,7 @@ public class SetScheduleDialog
 		AlertDialog.Builder builder = new AlertDialog.Builder(context);
 		builder.setView(view);
 		builder.setCancelable(true);
-		builder.setTitle(R.string.managemedication_schedule);
+		//builder.setTitle(R.string.managemedication_schedule);
 
 		builder.setOnCancelListener(new DialogInterface.OnCancelListener()
 		{
@@ -70,14 +80,18 @@ public class SetScheduleDialog
 			}
 		});
 
-		initSpinner();
+		initScheduleSpinner();
 		initCheckBoxes();
+        initCalendar();
+        initDaySpinner();
+        initNumberPicker();
 
 		alert = builder.create();
+        alert.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		alert.show();
 	}
 
-	private void initSpinner()
+	private void initScheduleSpinner()
 	{
 		ScheduleTypeSpinnerAdapter spinnerAdapter = new ScheduleTypeSpinnerAdapter(context);
 
@@ -106,6 +120,64 @@ public class SetScheduleDialog
 		cbDays[6].setOnCheckedChangeListener(onCbDaysCheckedChangeListener);
 		cbDays[7].setOnCheckedChangeListener(onCbDaysCheckedChangeListener);
 	}
+
+    private void initCalendar()
+    {
+        Calendar calendar = Calendar.getInstance();
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        selectedCalendarDays = new HashMap<Integer, Boolean>();
+        TableLayout tableLayout = (TableLayout) view.findViewById(R.id.vwCalendar);
+        int totalViews = 0;
+        for(int rowNum = 0; rowNum < 5; rowNum++)
+        {
+            ViewGroup currentGroup = (ViewGroup) tableLayout.getChildAt(rowNum);
+            for(int viewNum = 0; viewNum < 7; viewNum++)
+            {
+                try
+                {
+                    View view = currentGroup.getChildAt(viewNum);
+                    btnCalendarDays[totalViews] = view;
+                    if(totalViews + 1 == dayOfMonth)
+                    {
+                        view.setBackgroundResource(R.drawable.selector_reddotbg);
+                        ((Button) view).setTextColor(Color.parseColor("#FFFFFF"));
+                        selectedCalendarDays.put(dayOfMonth, true);
+                    }
+                    btnCalendarDays[totalViews].setOnClickListener(onCalendarDayClickedListener);
+                    totalViews++;
+                }
+                catch(ArrayIndexOutOfBoundsException e)
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    private void initDaySpinner()
+    {
+        StartDaySpinnerAdapter spinnerAdapter = new StartDaySpinnerAdapter(context);
+
+        spStartDay = (Spinner) view.findViewById(R.id.spStartDay);
+        spStartDay.setAdapter(spinnerAdapter);
+    }
+
+    private void initNumberPicker()
+    {
+        String[] nums = new String[31];
+
+        for(int i=0; i<nums.length; i++)
+        {
+            nums[i] = Integer.toString(i+1);
+        }
+        NumberPicker np = (NumberPicker) view.findViewById(R.id.np);
+        np.setMaxValue(nums.length);
+        np.setMinValue(1);
+        np.setWrapSelectorWheel(false);
+        np.setDisplayedValues(nums);
+
+    }
 
 	AdapterView.OnItemSelectedListener onScheduleTypeSelectedListener = new AdapterView.OnItemSelectedListener()
 	{
@@ -146,8 +218,17 @@ public class SetScheduleDialog
 				}
 				break;
 			case MedicationSchedule.TYPE_MONTHLY:
-				viewFlipper.setDisplayedChild(1);
+                if(viewFlipper.getDisplayedChild() != 1)
+                {
+                    viewFlipper.setDisplayedChild(1);
+                }
 				break;
+            case MedicationSchedule.TYPE_X_DAYS:
+                if(viewFlipper.getDisplayedChild() != 2)
+                {
+                    viewFlipper.setDisplayedChild(2);
+                }
+                break;
 			}
 
 			ignoreCheckBoxChange = false;
@@ -181,6 +262,28 @@ public class SetScheduleDialog
 		}
 	};
 
+    View.OnClickListener onCalendarDayClickedListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view) {
+            Button button = (Button) view;
+            int buttonNumber = Integer.parseInt(button.getText().toString());
+            if(selectedCalendarDays.get(buttonNumber) == null)
+            {
+                view.setBackgroundResource(R.drawable.selector_reddotbg);
+                ((Button) view).setTextColor(Color.parseColor("#FFFFFF"));
+                selectedCalendarDays.put(buttonNumber, true);
+            }
+            else if(selectedCalendarDays.get(buttonNumber) == true)
+            {
+                view.setBackgroundResource(R.drawable.selector);
+                ((Button) view).setTextColor(Color.parseColor("#000000"));
+                selectedCalendarDays.remove(buttonNumber);
+            }
+        }
+
+    };
+
 	class ScheduleTypeSpinnerAdapter extends ArrayAdapter<String>
 	{
 		LayoutInflater inflater;
@@ -207,7 +310,8 @@ public class SetScheduleDialog
 		public View getView(int position, View convertView, ViewGroup parent)
 		{
 			TextView view = (TextView) inflater.inflate(R.layout.sherlock_spinner_dropdown_item, null);
-
+            view.setTextSize(TypedValue.COMPLEX_UNIT_SP, 23);
+            view.setTextColor(view.getResources().getColor(R.color.card_title));
 			view.setText(strings[position]);
 
 			return view;
@@ -217,11 +321,55 @@ public class SetScheduleDialog
 		public View getDropDownView(int position, View convertView, ViewGroup parent)
 		{
 			View view = inflater.inflate(R.layout.dialog_setschedule_types, null);
-			TextView tvScheduleType = (TextView) view.findViewById(R.id.tvScheduleType);;
+			TextView tvScheduleType = (TextView) view.findViewById(R.id.tvScheduleType);
 
 			tvScheduleType.setText(strings[position]);
 
 			return view;
 		}
 	}
+
+    class StartDaySpinnerAdapter extends ArrayAdapter<String>
+    {
+        LayoutInflater inflater;
+        int preferredHeight;
+
+        String[] strings;
+
+        public StartDaySpinnerAdapter(Context context)
+        {
+            super(context, R.layout.sherlock_spinner_dropdown_item);
+
+            this.inflater = LayoutInflater.from(context);
+            this.preferredHeight = Utils.convertDpToPixel(48, context.getResources());
+            this.strings = context.getResources().getStringArray(R.array.startDays);
+        }
+
+        @Override
+        public int getCount()
+        {
+            return strings.length;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            TextView view = (TextView) inflater.inflate(R.layout.sherlock_spinner_dropdown_item, null);
+            //view.setTextColor(view.getResources().getColor(R.color.card_title));
+            view.setText(strings[position]);
+
+            return view;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent)
+        {
+            View view = inflater.inflate(R.layout.dialog_setschedule_types, null);
+            TextView tvStartDay = (TextView) view.findViewById(R.id.tvScheduleType);
+
+            tvStartDay.setText(strings[position]);
+
+            return view;
+        }
+    }
 }
